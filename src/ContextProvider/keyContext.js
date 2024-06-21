111d63cc6b3969ef5ce0c3a40e4cf55224bc6c0a
Chirag Vaviya <79510865+chirag8850@users.noreply.github.com>
2024-06-21 19:18:15 +0530

    Update keyContext.js

--- src/ContextProvider/keyContext.js
+++ src/ContextProvider/keyContext.js
@@ -1,14 +1,12 @@
 "use client";
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState } from "react";
 
 export const KeyContext = createContext();
 
 export function KeyContextProvider({ children }) {
-  const [openAiKey, setOpenAiKey] = useState("");
-
-  useEffect(() => {
-    setOpenAiKey(process.env.OPENAI_KEY);
-  }, []);
+  const [openAiKey, setOpenAiKey] = useState(
+    "sk-proj-M18sKlibwUchF0TB862ST3BlbkFJu276EEE0ZkpftdCIcCLu"
+  ); // Replace with your actual API key
 
   return (
     <KeyContext.Provider value={{ openAiKey, setOpenAiKey }}>
